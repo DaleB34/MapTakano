b0763813da3b799dfce4bfd82fce0cefc8f72b51
91112021 <dalebolido@gmail.com>
2023-10-20 13:18:32 -0700

    Changed starting position of the map to Valley View

--- app/src/main/java/com/example/myapplication/MapFragment.java
+++ app/src/main/java/com/example/myapplication/MapFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,21 +24,30 @@ public class MapFragment extends Fragment {
     {
         View view = inflater.inflate(R.layout.fragment_map,container,false);
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.MY_MAP);
+
+        //what happens when user clicks on the map
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
-                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                //setting starting point of the project
+                CameraUpdate point = CameraUpdateFactory.newLatLngZoom(new LatLng(33.9312, -117.1928), 14);
+                googleMap.moveCamera(point);
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener()
+                {
+
                     @Override
-                    public void onMapClick(@NonNull LatLng latLng) {
+                    public void onMapClick(@NonNull LatLng latLng)
+                    {
                         MarkerOptions markerOptions = new MarkerOptions();
                         markerOptions.position(latLng);
                         markerOptions.title(latLng.latitude + " KG " + latLng.longitude);
                         googleMap.clear();
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,20));
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,5));
                         googleMap.addMarker(markerOptions);
 
                     }
-                });
+                }
+                );
             }
         });
         return view;
