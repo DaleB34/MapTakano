665869ca696bc6184ae1a65fdd77600d24831da8
DaleB34 <dalebolido@gmail.com>
2023-10-29 15:42:07 -0700

    Added cshs, mvhs, and vdlhs markers

--- app/src/main/java/com/example/myapplication/MapFragment.java
+++ app/src/main/java/com/example/myapplication/MapFragment.java
@@ -33,18 +33,55 @@ public class MapFragment extends Fragment {
 
                 //setting starting point of the project
                 //and other things to make
-                CameraUpdate point = CameraUpdateFactory.newLatLngZoom(new LatLng(33.9312, -117.1928), 14);
+                CameraUpdate point = CameraUpdateFactory.newLatLngZoom(new LatLng(33.9627, -117.2580), 10);
                 gMap.moveCamera(point);
                 gMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(getActivity()));
+
+                //all school markers
 
                 final LatLng vvhsLatLng = new LatLng(33.9312,-117.1928);
                 Marker vvhs = gMap.addMarker(
                         new MarkerOptions()
                                 .position(vvhsLatLng)
-                                .title("Valley View High School")
-                                .snippet("Future Events:\nMrs.Rioux gets a millon dollars")
+                                .title("Valley View High School\n13135 Nason St.")
+                                .snippet("⚫ Saturday Innovation Camp -November 4th" +
+                                        "\n⚫ Veteran's Day -November 10th" +
+                                        "\n⚫ Thanksgiving Break -November 20th - 24th")
                 );
-                vvhs.showInfoWindow();
+
+                final LatLng cshsLatLng = new LatLng(33.9624422, -117.2584702);
+                Marker cshs = gMap.addMarker(
+                        new MarkerOptions()
+                                .position(cshsLatLng)
+                                .title("Canyon Springs High School\n23100 Cougar Canyon Dr.")
+                                .snippet("⚫ Cougar-Ween Costume Contest -October 31st" +
+                                        "\n⚫ Marching Band Competition -November 1st" +
+                                        "\n⚫ Coffee With Counselors -November 2nd" +
+                                        "\n⚫ Winter Sports Parent Meeting -November 2nd" +
+                                        "\n⚫ Marching Band Competition -November 7th")
+                );
+
+                final LatLng mvhsLatLng = new LatLng(33.9248377,-117.2561141);
+                Marker mvhs = gMap.addMarker(
+                        new MarkerOptions()
+                                .position(mvhsLatLng)
+                                .title("Moreno Valley High School\n23300 Cottonwood Ave.")
+                                .snippet("⚫ Band Competition -November 1st" +
+                                        "\n⚫ Dia De Los Muertos Performance -November 1st" +
+                                        "\n⚫ CTE Trade Show Field Trip -November 3rd" +
+                                        "\n⚫ Choir Festival -November 7th" +
+                                        "\n⚫ 9th Grade AVID Trip -November 7th")
+                );
+
+                final LatLng vdlhsLatLng = new LatLng(33.9004429,-117.2083632);
+                Marker vdlhs = gMap.addMarker(
+                        new MarkerOptions()
+                                .position(vdlhsLatLng)
+                                .title("Vista Del Lago High School\n15150 Lasselle St.")
+                                .snippet("⚫ Baseball Tryouts -November 1st" +
+                                        "\n⚫ \"She Kills Monsters\" Play -November 2nd" +
+                                        "\n⚫ \"She Kills Monsters\" Play -November 3rd")
+                );
 
 
                 //onclick map functionality
